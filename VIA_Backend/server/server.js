d91471f33c14c6471da812c953515538b44e069b
Vikash Yadav <vikashec62@gmail.com>
2026-03-24 21:26:23 +0530

    Update route paths in server .js

--- VIA_Backend/server/server.js
+++ VIA_Backend/server/server.js
@@ -1,81 +1,56 @@
-require("dotenv").config({ path: "./server/.env" })
+require("dotenv").config({ path: "./server/.env" });
 
-const http = require("http")
-const express = require("express")
-const cors = require("cors")
-const connectDB = require("./config/DB_conn")
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/DB_conn");
 
-const authRoutes = require("./routes/authRoutes")
-const chatRoutes = require("./routes/chatRoutes")
-const UserRouter  = require("./routes/userRoute")
-const agoraRoutes = require("./routes/agoraRoutes")
+const authRoutes = require("./routes/authRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const UserRouter = require("./routes/userRoute");
+const agoraRoutes = require("./routes/agoraRoutes");
+const chatSocket = require("./sockets/chatSocket");
 
-const app = express()
+const chatSocket = require("./sockets/chatSocket");
 
-
-// Connect Database
-connectDB()
+const app = express();
 
 // Middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+// Connect Database
+connectDB();
 
-
-
-const PORT = process.env.PORT || 5000
-
-
-
-
-
-
-
-
-
+const PORT = process.env.PORT || 5000;
 
 // create http server
-const server = http.createServer(app)
-const { Server } = require("socket.io")
+const server = http.createServer(app);
+const { Server } = require("socket.io");
 
 // create socket server
-const io = new Server(server,{
-  cors:{
-    origin:"http://localhost:5173"
-  }
-})
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
 
 // import socket logic
-const chatSocket = require("./sockets/chatSocket")
 
 // run socket logic
-chatSocket(io)
+chatSocket(io);
 
 // start server
-server.listen(PORT,()=>{
-  console.log(`🚀 Server running on port ${PORT}`)
-})
+server.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
 
 
+app.use("/api/auth", authRoutes);
 
+app.use("/api/chat", chatRoutes);
 
+app.use("/api/user", UserRouter);
 
+//Agora Routers
 
-
-
-app.use("/")
-
-
-app.use("/api/auth", authRoutes)
-
-
-
-app.use("/api/chat", chatRoutes)
-
-app.use("/api/user",UserRouter)
-
-
-
-//Agora Routers 
-
-
-app.use("/api/agora", agoraRoutes)
+app.use("/api/agora", agoraRoutes);
