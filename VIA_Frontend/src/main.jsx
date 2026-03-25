fe0ee420e5553640edd8eb0728c201d5f01f6811
Vikash Yadav <vikashec62@gmail.com>
2026-03-25 23:10:44 +0530

    Use HashRouter insted of BrowserRouter

--- VIA_Frontend/src/main.jsx
+++ VIA_Frontend/src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import App from "./app/App"
 import "./styles/index.css";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
