3abeed2d3a00addb70405d603b45674c5694e3ea
Vikash Yadav <vikashec62@gmail.com>
2026-03-26 17:58:10 +0530

    update the server.js path

--- VIA_Backend/server/server.js
+++ VIA_Backend/server/server.js
@@ -31,7 +31,7 @@ const { Server } = require("socket.io");
 // create socket server
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://via-frontend2.onrender.com",
   },
 });
 
